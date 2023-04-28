01fd78bee9226a9282746dd64be803e0577f5439
Carlos Rafael Contreras <crcontreras@applaudostudios.com>
2023-04-28 02:19:25 -0600

    Changes to git ignore

--- src/test/java/com/madrid/madrides/pages/HomePageTest.java
+++ src/test/java/com/madrid/madrides/pages/HomePageTest.java
@@ -14,27 +14,15 @@ public class HomePageTest extends BaseTest {
     public void setupPages(){
         this.homePage = new HomePage(driver);
     }
+
     @Test(priority = 1,groups = "homePageGroup")
     public void madridESWorkflow() throws InterruptedException{
         System.out.println("Executing Test 1 of HomePage\n");
-//        MadridESPage madridESPage = new MadridESPage(driver);
-        String expectedURL="https://www.madrid.es/portal/site/munimadrid";
-        String titleText;
-        Assertion assertTitle  = new Assertion();
 
         System.out.println("\n ********** Executing Step 1 **********");
         homePage.navigateTo();
-//        madridESPage.clickAcceptCookies();
-//        titleText = madridESPage.getTwitterIframeTitle();
-//        assertTitle.assertEquals("Tweets de @MADRID", titleText);
 
-        waitTime();
         System.out.println("Finishing Test 1 of HomePage\n");
     }
 
-    public void waitTime() throws InterruptedException {
-        synchronized (driver){
-            driver.wait(2000);
-        }
-    }
 }
